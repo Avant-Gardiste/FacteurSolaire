b461654def93a99cb47b22698281bbd6cc88a94e
El Mehdi TAFIK <58373039+Avant-Gardiste@users.noreply.github.com>
2024-05-24 14:39:09 +0200

    Add constantes.h

---
+++ constantes.h
@@ -0,0 +1,20 @@
+#ifndef DEFINECONSTANTS
+#define DEFINECONSTANTS
+
+const double _PI              = 3.141592f; /**< TODO: describe */
+const double _StefanBoltzmann = 5.67e-8; //0.0000000567f; /**< TODO: describe */  5.67e-8
+const double _T0              = 293.15;
+const double _NUV             = 0.2f; /**< TODO: describe */
+const double _EV              = 70000000000.0f; /**< TODO: describe */
+const double _ROV             = 2530; /**< TODO: describe */
+const double _G               = 9.81; /**< TODO: describe */
+const double _PATM            = 101325; /**< TODO: describe */
+const double _DPATM           = 0; /**< TODO: describe */
+const double _EEV             = 70000.0f; /**< TODO: describe */
+
+//Thermal breakage
+const double _Cmro            = 2000000; /**< TODO: describe */
+const double _DT              = 900; /**< TODO: describe */
+const double _CMROA           = 1300; /**< TODO: describe */
+
+#endif // DEFINECONSTANTS
